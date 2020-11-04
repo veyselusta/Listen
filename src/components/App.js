d2c5cd42b2162bf3e30de6e8f6ec6cec821f4782
Your Name <you@example.com>
2020-11-04 17:24:51 +0300

    app.js was changed and gitignore was created

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom";
 import "./app.css";
 
 export default function App() {
+  const [name, setName] = useState("")
+
+  const keep = (e)=>{setName(e.target.value)}
+
+  const match = ()=>{name=="open sesame" ? alert("You won") : alert("Try again")}
+
   return (
     <div className="App">
       <h2>What's the secret phrase?</h2>
 
-      <input type="text" placeholder="Super duper secret" />
+      <input 
+        type="text" 
+        placeholder="Super duper secret" /
+        onChange={keep}
+        >
+
+        <button onClick={match}>click</button>
 
       <p>
         Hint: It's <strong>open sesame</strong>
       </p>
+
     </div>
   );
 }
